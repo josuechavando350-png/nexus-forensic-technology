64efe556dfb69595ff6dc4b9f06f22a070394cf9
josuechavando350-png <josuechavando350@gmail.com>
2026-08-26 14:03:19 -0600

    feat: add capability coverage package

---
+++ packages/capabilities/__init__.py
@@ -0,0 +1,3 @@
+from .coverage import CAPABILITY_COVERAGE, CapabilityCoverage, catalog_progress_percent, implemented_capability_count
+
+__all__ = ["CAPABILITY_COVERAGE", "CapabilityCoverage", "catalog_progress_percent", "implemented_capability_count"]
